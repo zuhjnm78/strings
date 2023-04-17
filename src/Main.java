8618fa47bf058b936e56ef78b45f69320b5f58a5
oleghenkel <oleg.henkel75@gmail.com>
2023-04-17 15:44:00 +0200

    strings1

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        //task3();
+        //task4();
+
+    }
+
+    public static void task1() {
+
+        System.out.println("Задача 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("ФИО сотрудника — " + fullName);
+    }
+
+    public static void task2() {
+
+        System.out.println("Задача 2");
+
+
+    }
+}
